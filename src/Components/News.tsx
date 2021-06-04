937c957546268592019e16943957583864f92dac
jhordancamacho <jhordan.camacho@gmail.com>
2021-06-03 22:34:15 -0500

    noticias listo

--- src/Components/News.tsx
+++ src/Components/News.tsx
@@ -1,22 +1,33 @@
 import { useEffect, useState } from "react";
+import "./news.css";
 
 function News() {
-  //const [Noticias, setNoticias] = useState([]);
-  //useEffect(() => {
-  //fetch(
-  //"http://api.mediastack.com/v1/news?access_key=7e4ec37df17151d47554a7f64556cec4"
-  //    )
-  //    .then((Response) => Response.json())
-  //  .then((data) => setNoticias(data.results));
-  //}, []);
+  const [Noticias, setNoticias] = useState([]);
+  useEffect(() => {
+    fetch(
+      "http://newsapi.org/v2/top-headlines?country=co&apiKey=939167e7872649b9a97697a91db03fce&languaje=es"
+    )
+      .then((Response) => Response.json())
+      .then((data) => setNoticias(data.articles));
+  }, []);
 
   return (
     <div className="news">
-      {/*{//Noticias.map((item : []) => (
-      //  <div className="boxx">
-          
-       // </div>
-      ))};*/}
+      {Noticias.map((item: any) => (
+        <div className="card">
+          <div className="banner">
+            <p className="source">{item.source.name}</p>
+            <img className="banner-img" src={item.urlToImage} alt="imagen" />
+          </div>
+          <div className="character">
+            <h2 className="title">{item.title}</h2>
+            <p className="description">{item.description}</p>
+            <a className="link" href={item.url} target="_blank">
+              Ver más
+            </a>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
--- src/Components/Search.tsx
+++ src/Components/Search.tsx
@@ -1,12 +1,24 @@
-//import React, { Fragment, useState } from "react";
+import React, { Fragment, KeyboardEvent, useState } from "react";
 import lupa from "../images/lupa.png";
 import "./search.css";
-//const [Contenido, setContenido] = useState("");
 
 function Search() {
+  const apiBas =
+    "https://newsapi.org/v2/everything?q=nueva-york&apiKey=939167e7872649b9a97697a91db03fce";
+  const [Contenido, setContenido] = useState("");
+
+  const enterPress = (event: KeyboardEvent) => {
+    if (event.key === "Enter") {
+      console.log("do validate  " + Contenido);
+    }
+  };
+
   return (
     <div className="search">
       <input
+        value={Contenido}
+        onChange={({ target: { value } }) => setContenido(value)}
+        onKeyDown={enterPress}
         name="barra de busqueda"
         placeholder="Inicia tu busqueda"
         autoFocus
